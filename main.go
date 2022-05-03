79535ac171197b1797708536733623d2bd5c59f9
Mauro Quinteros <quinterosmauro0599@gmail.com>
2022-05-02 23:18:34 -0500

    [ADD] :white_check_mark: Keywords

--- main.go
+++ main.go
@@ -3,37 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	modulo2 := 10 % 2
+	// defer ejecuta una línea de código antes de que se termine de ejecutar el programa
+	// Se utiliza para cerrar una conexión a una base de datos, para cerrar un archivo.
+	defer fmt.Println("Hello")
+	fmt.Println("World")
 
-	// 1° forma (con condición determinada)
-	switch modulo2 {
-	case 0:
-		fmt.Println("El número es par")
-	case 1:
-		fmt.Println("El número es impar")
-	default:
-		fmt.Println("No se puede calcular")
+	for i := 0; i < 10; i++ {
+		fmt.Println("Number", i)
+
+		if i == 2 {
+			fmt.Println("Number is odd")
+			continue // Se salta el resto de la línea y no detiene el ciclo for
+		}
+
+		if i == 8 {
+			fmt.Println("Number is 8")
+			break // Se detiene el ciclo for
+		}
 	}
-
-	// 2° forma (con condición determinada)
-	switch modulo1 := 10 % 2; modulo1 {
-	case 0:
-		fmt.Println("El número es par")
-	case 1:
-		fmt.Println("El número es impar")
-	default:
-		fmt.Println("No se puede calcular")
-	}
-
-	// 3° forma (sin condición determinada)
-	value := 80
-	switch {
-	case value > 100:
-		fmt.Println("El valor es mayor a 100")
-	case value < 0:
-		fmt.Println("El valor es menor a 0")
-	default:
-		fmt.Println("El valor no cumple ninguna condicion")
-	}
-
 }
