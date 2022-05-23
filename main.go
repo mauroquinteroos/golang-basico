ce60ffdb005ad22204d527beed2ebe2a94464d92
Mauro Quinteros <quinterosmauro0599@gmail.com>
2022-05-23 13:54:33 -0500

    [ADD] :white_check_mark: Modificadores de acceso con punteros

--- main.go
+++ main.go
@@ -1,66 +1,28 @@
 package main
 
-import (
-	"fmt"
-	pn "golang_basico/mypackage"
-)
+import "fmt"
 
-type pc struct {
-	ram   int
-	disk  int
-	brand string
+type cuadrado struct {
+	base float64
 }
 
-// Receiver functions with Value receiver
-func (myPc pc) ping() {
-	myPc.brand = "Apple"
-	fmt.Println("local object: ", myPc)
+func (c cuadrado) area() float64 {
+	return c.base * c.base
 }
 
-// Receiver functions with Pointer receiver
-func (myPc *pc) duplicateRam() {
-	myPc.disk *= 2
-	fmt.Println("local object: ", myPc)
+type rectangulo struct {
+	base   float64
+	altura float64
 }
 
-// Stringers (Personalizar el output de un Struct)
-func (myPc pc) String() string {
-	return fmt.Sprintf("Tengo %d GB RAM, %d GB Disco y es una %s", myPc.ram, myPc.disk, myPc.brand)
+func (r rectangulo) area() float64 {
+	return r.base * r.altura
 }
 
 func main() {
-	a := 50
-	// b será el puntera de a (la dirección del espacio en memoria donde se guarda a)
-	b := &a
+	cuadrado1 := cuadrado{base: 5}
+	fmt.Println("Area: ", cuadrado1.area())
 
-	fmt.Println("a:", a)
-	fmt.Printf("%p\n", &a)
-	fmt.Println("b: ", b)
-
-	fmt.Println("--------------------------------")
-
-	*b = 100
-	fmt.Println("b: ", *b)
-	fmt.Println("a:", a)
-
-	fmt.Println("--------------------------------")
-
-	myPc := pc{ram: 4, disk: 500, brand: "Dell"}
-	myPc.ping()
-	fmt.Println("original object: ", myPc)
-
-	fmt.Println("--------------------------------")
-
-	myPc.duplicateRam()
-	fmt.Println("original object: ", myPc)
-
-	fmt.Println("--------------------------------")
-
-	person1 := pn.Person{Name: "Mauro", Doc: 77382771, Age: 23}
-	fmt.Println("person1: ", person1.Print())
-	person1.AddAge()
-	fmt.Println("person1: ", person1.Print())
-
-	fmt.Println("--------------------------")
-
+	rectangulo1 := rectangulo{base: 5, altura: 10}
+	fmt.Println("Area: ", rectangulo1.area())
 }
