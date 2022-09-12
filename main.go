cceb4a373f5b6967e6b98efbc5d869c77bcaab61
Mauro Quinteros <quinterosmauro0599@gmail.com>
2022-09-11 19:49:21 -0500

    [REVIEW] Packages examples

--- main.go
+++ main.go
@@ -4,30 +4,35 @@ import (
 	"fmt"
 )
 
+func message(text string, c chan string) {
+	c <- text
+}
+
 func main() {
-	jobs := make(chan int, 5)
-	done := make(chan bool)
+	c := make(chan string, 4)
+	c <- "hello"
+	c <- "world"
+	fmt.Println("len: ", len(c))
+	fmt.Println("cap: ", cap(c))
 
-	go func() {
-		for {
-			j, more := <-jobs
-			fmt.Println("job: ", j)
-			fmt.Println("is there more: ", more)
-			if more {
-				fmt.Println("received job", j)
-			} else {
-				fmt.Println("received all jobs")
-				done <- true
-				return
-			}
-		}
-	}()
+	close(c)
 
-	for j := 1; j <= 5; j++ {
-		jobs <- j
-		fmt.Println("sent job", j)
+	for message := range c {
+		fmt.Println(message)
 	}
-	close(jobs)
-	fmt.Println("sent all jobs")
-	<-done
+
+	email1 := make(chan string)
+	email2 := make(chan string)
+
+	go message("message 1", email1)
+	go message("message 2", email2)
+
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <-email1:
+			fmt.Println("Email recibido de email1: ", msg1)
+		case msg2 := <-email2:
+			fmt.Println("Email recibido de email2: ", msg2)
+		}
+	}
 }
